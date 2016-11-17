f40c95c2a47a9599a6dc5ea189e6c4146e9f225e
ChrisPeleti <chrisyred@gmail.com>
2016-11-17 09:44:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const app = "I don't do much.";
+const React = require('react')
+
+class Tweet extends React.Component {
+  render() {
+    return (
+      <div className="tweet">
+        <img src="http://twitter.com/some-avatar.png" className="tweet__avatar" />
+        <div className="tweet__body">
+            <p>Were writing this tweet in JSX. Holy moly!</p>
+        </div>
+      </div>
+    );
+  }
+}
+module.exports = Tweet;
